e0585ba3f3e6bb84cab333e1f5b062127af38e0f
zxcvvxcz <pch330@naver.com>
2020-11-06 23:46:54 +0900

    test program for problem 2

---
+++ test.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    int n_origin = 5100;
+    int n;
+    int primes_size;
+    int i;
+    int *primes;
+    primes_size = 0;
+    n = n_origin;
+    while(!(n % 2)){
+        n /= 2;
+        primes_size++;
+    }
+    primes = (int *) calloc(primes_size, sizeof(int));
+    for(i = 0; i < primes_size; i++){
+        primes[i] = 2;
+    }
+    i = 3;
+    while(i <= floor(sqrt(n))){
+        if(!(n % i)){
+            n /= i;
+            primes_size++;
+            primes = (int *) realloc(primes, primes_size * sizeof(int));
+            primes[primes_size - 1] = i;
+        } else{
+            i += 2;
+        }
+
+    }
+    if(n != 1){
+        primes_size++;
+        primes = (int *) realloc(primes, primes_size * sizeof(int));
+        primes[primes_size - 1] = n;
+    }
+    printf("Prime factorization of %d: ", n_origin);
+    int product = 1;
+    for(i = 0; i < primes_size; i++){
+        printf("%d ", primes[i]);
+        product *= primes[i];
+        if(i + 1 != primes_size){
+            printf("x ");
+        }
+    }
+    printf("\nProduct from prime factorization: %d\n", product);
+
+
+
+
+
+    return 0;
+}
